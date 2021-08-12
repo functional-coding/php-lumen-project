7027b7c07d316369b1f5ead8788fb1b998f33549
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-12 15:00:51 +0900

    support auto-scan commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,33 @@ class Kernel extends ConsoleKernel
     protected $commands = [
     ];
 
+    protected function getCommands()
+    {
+        $path = base_path('app/Console/Commands');
+        $dir = new \RecursiveDirectoryIterator($path);
+        $list = new \RecursiveIteratorIterator($dir);
+        $files = new \RegexIterator($list, '/.+\.php$/');
+        $commands = parent::getCommands();
+
+        foreach ($files as $file) {
+            require_once $file->getPathname();
+        }
+
+        $classes = new \RegexIterator(new \ArrayIterator(get_declared_classes()), '/Commands/');
+        $classes = iterator_to_array($classes);
+        sort($classes);
+
+        foreach ($classes as $class) {
+            $reflect = new \ReflectionClass($class);
+
+            if (!$reflect->isAbstract()) {
+                array_push($commands, $class);
+            }
+        }
+
+        return $commands;
+    }
+
     /**
      * Define the application's command schedule.
      */
