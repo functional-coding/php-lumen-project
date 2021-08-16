8127555aa9f18292d38bb237079fe9b35c90ff68
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-17 02:35:51 +0900

    fix: class name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controller;
-use App\Services\User\UserFindingService;
+use App\Services\User\UserFindService;
 
 class UserController extends Controller
 {
     public static function show()
     {
-        return [UserFindingService::class];
+        return [UserFindService::class];
     }
 }
