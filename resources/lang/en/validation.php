76a209fdcd06274f4575899fddf1513b3553c2fa
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-17 14:03:46 +0900

    update resources/lang/en/validation.php

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -129,7 +129,6 @@ return [
     'base64' => ':attribute must to be base64 encoded string.',
     'null' => ':attribute must not exist.',
     'null_if' => ':attribute must not exist.',
-    'not_in' => 'The selected :attribute is invalid.',
     'not_null' => ':attribute must exist.',
     'not_null_if' => ':attribute must exist.',
     'false' => ':attribute must be false.',
